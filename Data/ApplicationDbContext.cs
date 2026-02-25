3e4696c604f86f5463bce9bac95bd15d8d1eec8d
kbravrl <kubravarol499@gmail.com>
2026-02-25 10:16:49 +0300

    register room entities in ApplicationDbContext

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,3 +1,4 @@
+using HotelWeb.Models;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -5,5 +6,32 @@ namespace HotelWeb.Data
 {
     public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
     {
+        public DbSet<RoomType> RoomTypes { get; set; }
+        public DbSet<Room> Rooms { get; set; }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<RoomType>(e =>
+            {
+                e.Property(x => x.Name).HasMaxLength(80).IsRequired();
+                e.Property(x => x.Description).HasMaxLength(500);
+                e.Property(x => x.MaxCapacity).IsRequired();
+            });
+
+            builder.Entity<Room>(e =>
+            {
+                e.Property(x => x.RoomNumber).IsRequired();
+                e.HasIndex(x => x.RoomNumber).IsUnique();
+
+                e.Property(x => x.BasePrice).HasColumnType("numeric(10,2)");
+                e.Property(x => x.Capacity).IsRequired();
+
+                e.HasOne(x => x.RoomType)
+                 .WithMany(x => x.Rooms)
+                 .HasForeignKey(x => x.RoomTypeId)
+                 .OnDelete(DeleteBehavior.Restrict);
+            });
+        }
     }
 }
