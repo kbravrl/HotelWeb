8a733860e0dbf85eb4846a2a58fbf16a486db649
kbravrl <kubravarol499@gmail.com>
2026-02-26 09:58:31 +0300

    add housekeeping task repository

---
+++ Repositories/HousekeepingTaskRepository.cs
@@ -0,0 +1,27 @@
+﻿using HotelWeb.Data;
+using HotelWeb.Enums;
+using HotelWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace HotelWeb.Repositories;
+public class HousekeepingTaskRepository(ApplicationDbContext db)
+    : IHousekeepingTaskRepository
+{
+    public async Task AddAsync(HousekeepingTask task)
+        => await db.HousekeepingTasks.AddAsync(task);
+
+    public Task<HousekeepingTask?> GetByIdAsync(int id)
+        => db.HousekeepingTasks
+            .Include(t => t.Room)
+            .FirstOrDefaultAsync(t => t.Id == id);
+
+    public Task<List<HousekeepingTask>> GetOpenTasksAsync()
+        => db.HousekeepingTasks
+            .Include(t => t.Room)
+            .Where(t => t.Status != HousekeepingTaskStatus.Done)
+            .OrderBy(t => t.CreatedAt)
+            .ToListAsync();
+
+    public Task SaveChangesAsync()
+        => db.SaveChangesAsync();
+}
---
+++ Repositories/IHousekeepingTaskRepository.cs
@@ -0,0 +1,11 @@
+﻿using HotelWeb.Models;
+
+namespace HotelWeb.Repositories;
+
+public interface IHousekeepingTaskRepository
+{
+    Task AddAsync(HousekeepingTask task);
+    Task<HousekeepingTask?> GetByIdAsync(int id);
+    Task<List<HousekeepingTask>> GetOpenTasksAsync();
+    Task SaveChangesAsync();
+}
