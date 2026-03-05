b8dcb7060bca28f9b12e39b6809395347392c37d
kbravrl <kubravarol499@gmail.com>
2026-03-05 16:38:22 +0300

    add bootstrap modal and javascript toastr

---
+++ wwwroot/js/notification.js
@@ -0,0 +1,26 @@
+﻿toastr.options = {
+    "closeButton": true,
+    "debug": false,
+    "newestOnTop": true,
+    "progressBar": true,
+    "positionClass": "toast-top-right",
+    "preventDuplicates": false,
+    "onclick": null,
+    "showDuration": "300",
+    "hideDuration": "1000",
+    "timeOut": "5000",
+    "extendedTimeOut": "1000",
+    "showEasing": "swing",
+    "hideEasing": "linear",
+    "showMethod": "fadeIn",
+    "hideMethod": "fadeOut"
+};
+
+window.ShowToastr = function (type, message) {
+    if (type == "success") {
+        toastr.success(message);
+    }
+    if (type == "error") {
+        toastr.error(message);
+    }
+}
