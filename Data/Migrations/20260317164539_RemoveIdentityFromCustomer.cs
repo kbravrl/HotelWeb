9e09d88415336bbd996b5b5d870b0eafa2832af2
Kübra Varol <kubravarol499@gmail.com>
2026-03-17 19:46:49 +0300

    remove identity number from customer table

---
+++ Data/Migrations/20260317164539_RemoveIdentityFromCustomer.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HotelWeb.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveIdentityFromCustomer : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IdentityNumber",
+                table: "Customers");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "IdentityNumber",
+                table: "Customers",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
